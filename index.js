6dda6489279c48cd082eca6b19eceef2e2cf0ede
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2026-01-14 20:48:50 -0600

    start working on removing items from watchlist

--- index.js
+++ index.js
@@ -3,30 +3,40 @@ const APIKey = "3eed1eec"
 let movieResults = document.querySelector(".movie-results")
 const exploring = document.querySelector(".exploring")
 const empty = document.querySelector(".empty")
-const watchlist = document.querySelector(".watchlist")
+const watchlistEl = document.querySelector(".watchlist")
 const userQuery = document.querySelector("#search")
 const searchBtn = document.querySelector("#search-btn")
 
 let tempMovies = {}
 
-const moviesObj = localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist")) : {};
+const moviesObj = localStorage.getItem("watchlist") ? localStorage.getItem("watchlist") : localStorage.setItem("watchlist", JSON.stringify({}));
 
 // Event Listeners
 
-searchBtn.addEventListener("click", (e) => {
+if (searchBtn != null) {
+    searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
     searchMovies(userQuery.value)
 })
+}
+
 
 // Functions
 
 function checkWatchlist() {
-    if (localStorage.getItem("watchlist") == null) {
-        empty.style.display = "flex"
-        watchlist.style.display = "none"
+    if (localStorage.getItem("watchlist") === "null") {
+        if (empty != null && watchlistEl != null) {
+            empty.style.display = "flex"
+            watchlistEl.style.display = "none"
+        }
+        
+        
     } else {
-        empty.style.display = "none"
-        watchlist.style.display = "flex"
+        if (empty != null && watchlistEl != null) {
+            empty.style.display = "none"
+            watchlistEl.style.display = "flex"
+        }
+        displayWatchList()
     }
 }
 
@@ -62,18 +72,48 @@ function populateMovieResults(data) {
 
 function addToWatchList(title) {
 
-    let movieToAdd = JSON.parse(localStorage.getItem(title))
+    let watchlist = JSON.parse(localStorage.getItem("watchlist"))
+    watchlist[title] = JSON.stringify(tempMovies[title])
+    
+    localStorage.setItem("watchlist", JSON.stringify(watchlist))  
+}
 
-    if (!movieToAdd) {
-        movieToAdd = tempMovies[title]
-        localStorage.setItem(title, JSON.stringify(movieToAdd))
+function removeFromWatchList(title) {
+    let watchlist = JSON.parse(localStorage.getItem("watchlist"))
+    // watchlist[title] = JSON.stringify(tempMovies[title])
+    delete watchlist.title
+    
+    localStorage.setItem("watchlist", JSON.stringify(watchlist))
+}
+
+function displayWatchList() {
+    let data = JSON.parse(localStorage.getItem("watchlist"))
+    // let watchlist = JSON.stringify(data)
+    populateWatchList(data)
+    
+}
+
+function populateWatchList(data) {
+    for (const key in data) {
+        item = JSON.parse(data[key])
+        watchlistEl.innerHTML += `
+        <div class="movie">
+            <img class="poster" src=${item.Poster} alt=${item.Title}>
+            <div class="movie-data">
+                <h2 class="movie-title">${item.Title}</h2>
+                <p class="movie-year">${item.Year}<p>
+                <p class="remove-from-watchlist" onclick="removeFromWatchList('${item.Title}')">
+                    <img class="add-icon" src="images/remove.svg">
+                        Remove from watchlist
+                </p>
+            </div>
+        </div>
+        `
     }
-
 }
 
 // Function calls
 
 
+window.addEventListener("load", checkWatchlist)
 
-
-checkWatchlist()
